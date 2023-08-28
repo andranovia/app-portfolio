412d8fd5f9832f8a11d303b75ce410fd5afdae34
Andra <yeromrenandra07@gmail.com>
2023-08-28 11:04:15 +0700

    deploy-app

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'], // Add 'react-dom' here
+      external: ['react-dom/client'], // Add 'react-dom' here
     },
   },
 });
